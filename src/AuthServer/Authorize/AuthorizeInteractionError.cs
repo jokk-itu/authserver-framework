0338753135fc390698fc4e323bcd598c4ac76430
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2024-12-05 23:06:56 +0100

    add: authorize error with request uri and client id for interaction redirection

---
+++ src/AuthServer/Authorize/AuthorizeInteractionError.cs
@@ -0,0 +1,5 @@
+﻿using AuthServer.Core.Request;
+
+namespace AuthServer.Authorize;
+
+public record AuthorizeInteractionError(string Error, string ErrorDescription, ResultCode ResultCode, string RequestUri, string ClientId) : ProcessError(Error, ErrorDescription, ResultCode);
