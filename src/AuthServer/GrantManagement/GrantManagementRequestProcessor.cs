09776832e399227394ed9ed8cb605a3e5415f110
joachim-kelsen <joachim@kelsen.nu>
2025-01-05 10:57:31 +0100

    add: grant management processor

---
+++ src/AuthServer/GrantManagement/GrantManagementRequestProcessor.cs
@@ -0,0 +1,22 @@
+using AuthServer.Core;
+using AuthServer.Core.Abstractions;
+using AuthServer.Core.Request;
+using AuthServer.Repositories.Abstractions;
+
+namespace AuthServer.GrantManagement;
+
+internal class GrantManagementRequestProcessor : IRequestProcessor<GrantManagementValidatedRequest, Unit>
+{
+    private readonly IAuthorizationGrantRepository _authorizationGrantRepository;
+
+    public GrantManagementRequestProcessor(IAuthorizationGrantRepository authorizationGrantRepository)
+    {
+        _authorizationGrantRepository = authorizationGrantRepository;
+    }
+    
+    public async Task<Unit> Process(GrantManagementValidatedRequest request, CancellationToken cancellationToken)
+    {
+        await _authorizationGrantRepository.RevokeGrant(request.GrantId, cancellationToken);
+        return Unit.Value;
+    }
+}
