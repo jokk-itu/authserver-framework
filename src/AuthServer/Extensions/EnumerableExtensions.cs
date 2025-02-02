e3f6c45d2da83e97558e9054bc2602e220378192
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-02-02 20:39:34 +0100

    update: rename to subset

--- src/AuthServer/Extensions/EnumerableExtensions.cs
+++ src/AuthServer/Extensions/EnumerableExtensions.cs
@@ -2,12 +2,12 @@
 internal static class EnumerableExtensions
 {
     /// <summary>
-    /// Source is the suspicious input, and ValidInput is the source of truth.
+    /// Is subSet of superSet.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    /// <param name="source"></param>
-    /// <param name="validInput"></param>
-    /// <returns>false if source contains any input that is not found in validInput or if source is empty</returns>
-    public static bool ExceptAny<T>(this IEnumerable<T> source, IEnumerable<T> validInput) =>
-        source.Except(validInput).Any();
+    /// <param name="subSet"></param>
+    /// <param name="superSet"></param>
+    /// <returns>false if subSet contains any input that is not found in superSet or if source is empty</returns>
+    public static bool IsSubset<T>(this IEnumerable<T> subSet, IEnumerable<T> superSet)
+        => subSet.Except(superSet).Any();
 }
