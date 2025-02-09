7d878d4ea9e1ada9b94829e9b13a2a1b0a983c19
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-02-09 12:15:48 +0100

    update: grant errors to return correct responses instead of bad request

--- src/AuthServer/GrantManagement/GrantManagementError.cs
+++ src/AuthServer/GrantManagement/GrantManagementError.cs
@@ -6,11 +6,11 @@ namespace AuthServer.GrantManagement;
 public class GrantManagementError
 {
     public static readonly ProcessError InvalidGrantId =
-        new(ErrorCode.InvalidRequest, "grant_id is required", ResultCode.BadRequest);
+        new(ErrorCode.InvalidRequest, "grant_id is required", ResultCode.NotFound);
 
-    public static readonly ProcessError UnexistingGrantId =
-        new(ErrorCode.InvalidRequest, "grant does not exist", ResultCode.BadRequest);
+    public static readonly ProcessError NotFoundGrantId =
+        new(ErrorCode.InvalidRequest, "grant does not exist", ResultCode.NotFound);
     
     public static readonly ProcessError InvalidGrant =
-        new(ErrorCode.InvalidGrant, "grant is issued to other client", ResultCode.BadRequest);
+        new(ErrorCode.InvalidGrant, "grant is issued to other client", ResultCode.Forbidden);
 }
