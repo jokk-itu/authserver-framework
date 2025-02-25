b5d38c1becffa6f8608e452f4bdd8b8c8f2fdbb3
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-02-25 22:37:20 +0100

    update: make userinteraction nullable if authorize is disabled

--- src/AuthServer/Options/ValidateUserInteractionOptions.cs
+++ src/AuthServer/Options/ValidateUserInteractionOptions.cs
@@ -1,5 +1,4 @@
-﻿using AuthServer.Core;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Options;
 
 namespace AuthServer.Options;
 
@@ -7,23 +6,22 @@ public class ValidateUserInteractionOptions : IValidateOptions<UserInteraction>
 {
     public ValidateOptionsResult Validate(string? name, UserInteraction options)
     {
-        if (string.IsNullOrEmpty(options.LoginUri) || !Uri.IsWellFormedUriString(options.LoginUri, UriKind.Absolute))
+        if (!string.IsNullOrEmpty(options.LoginUri) && !Uri.IsWellFormedUriString(options.LoginUri, UriKind.Absolute))
         {
             return ValidateOptionsResult.Fail($"{nameof(options.LoginUri)} is not specified");
         }
 
-        if (string.IsNullOrEmpty(options.ConsentUri) || !Uri.IsWellFormedUriString(options.ConsentUri, UriKind.Absolute))
+        if (!string.IsNullOrEmpty(options.ConsentUri) && !Uri.IsWellFormedUriString(options.ConsentUri, UriKind.Absolute))
         {
             return ValidateOptionsResult.Fail($"{nameof(options.ConsentUri)} is not specified");
         }
 
-        if (string.IsNullOrEmpty(options.AccountSelectionUri) || !Uri.IsWellFormedUriString(options.AccountSelectionUri, UriKind.Absolute))
+        if (!string.IsNullOrEmpty(options.AccountSelectionUri) && !Uri.IsWellFormedUriString(options.AccountSelectionUri, UriKind.Absolute))
         {
             return ValidateOptionsResult.Fail($"{nameof(options.AccountSelectionUri)} is not specified");
         }
 
-        if (string.IsNullOrEmpty(options.EndSessionUri) ||
-            !Uri.IsWellFormedUriString(options.EndSessionUri, UriKind.Absolute))
+        if (!string.IsNullOrEmpty(options.EndSessionUri) && !Uri.IsWellFormedUriString(options.EndSessionUri, UriKind.Absolute))
         {
             return ValidateOptionsResult.Fail($"{nameof(options.EndSessionUri)} is not specified");
         }
