9a9d4afb189e7ce2e7fafb80218b7169bd72cc25
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-02-25 21:29:44 +0100

    remove: static jwks and discovery registration and only rely on modules

--- src/AuthServer/Extensions/WebApplicationExtensions.cs
+++ src/AuthServer/Extensions/WebApplicationExtensions.cs
@@ -1,9 +1,6 @@
 ﻿using AuthServer.Core.Abstractions;
 using AuthServer.Endpoints;
-using AuthServer.Endpoints.Filters;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -20,29 +17,5 @@ public static class WebApplicationExtensions
             module.RegisterEndpoint(app);
             app.Logger.LogDebug("Registered endpoint {Endpoint}", nameof(module));
         }
-
-        IEndpointRouteBuilder endpointBuilder = app;
-
-        endpointBuilder
-            .MapMethods(".well-known/openid-configuration", ["GET"], DiscoveryDocumentEndpoint.HandleDiscoveryDocument)
-            .WithDisplayName("OpenIdConnect Configuration")
-            .WithName("OpenIdConnect Configuration")
-            .WithDescription("Endpoint to get the configuration")
-            .WithGroupName("Configuration")
-            .AddEndpointFilter(async (ctx, next) =>
-            {
-                ctx.HttpContext.Response.Headers.CacheControl = "max-age=86400, public, must-revalidate";
-                return await next(ctx);
-            })
-            .AddEndpointFilter<NoReferrerFilter>();
-
-        endpointBuilder
-            .MapMethods(".well-known/jwks", ["GET"], JwksDocumentEndpoint.HandleJwksDocument)
-            .WithDisplayName("OAuth JWKS")
-            .WithName("OAuth JWKS")
-            .WithDescription("Endpoint to get the jwks")
-            .WithGroupName("Configuration")
-            .AddEndpointFilter<NoCacheFilter>()
-            .AddEndpointFilter<NoReferrerFilter>();
     }
 }
