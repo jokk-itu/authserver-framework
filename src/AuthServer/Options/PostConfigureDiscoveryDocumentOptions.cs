7c03f9448bd6a319ffee88f3d30fedb4daac9660
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-02-26 18:35:05 +0100

    uipdate: do not support default values for signing properties

--- src/AuthServer/Options/PostConfigureDiscoveryDocumentOptions.cs
+++ src/AuthServer/Options/PostConfigureDiscoveryDocumentOptions.cs
@@ -11,35 +11,5 @@ internal class PostConfigureDiscoveryDocumentOptions : IPostConfigureOptions<Dis
 			.Where(x => !options.ScopesSupported.Contains(x))
 			.ToList()
 			.ForEach(options.ScopesSupported.Add);
-
-		if (options.IdTokenSigningAlgValuesSupported.Count == 0)
-		{
-			options.IdTokenSigningAlgValuesSupported = [JwsAlgConstants.RsaSha256];
-		}
-
-		if (options.RequestObjectSigningAlgValuesSupported.Count == 0)
-		{
-			options.RequestObjectSigningAlgValuesSupported = [JwsAlgConstants.RsaSha256];
-		}
-
-		if (options.UserinfoSigningAlgValuesSupported.Count == 0)
-		{
-			options.UserinfoSigningAlgValuesSupported = [JwsAlgConstants.RsaSha256];
-		}
-
-		if (options.TokenEndpointAuthSigningAlgValuesSupported.Count == 0)
-		{
-			options.TokenEndpointAuthSigningAlgValuesSupported = [JwsAlgConstants.RsaSha256];
-		}
-
-		if (options.IntrospectionEndpointAuthSigningAlgValuesSupported.Count == 0)
-		{
-			options.IntrospectionEndpointAuthSigningAlgValuesSupported = [JwsAlgConstants.RsaSha256];
-		}
-
-		if (options.RevocationEndpointAuthSigningAlgValuesSupported.Count == 0)
-		{
-			options.RevocationEndpointAuthSigningAlgValuesSupported = [JwsAlgConstants.RsaSha256];
-		}
 	}
 }
