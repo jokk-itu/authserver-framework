dbb49fbfb593a7106b90579637563da56e831bad
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-02-27 19:13:05 +0100

    add: constants for claim types

---
+++ src/AuthServer/Constants/ClaimTypeConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthServer.Constants;
+
+internal static class ClaimTypeConstants
+{
+    public const string Normal = "normal";
+
+    public static readonly string[] ClaimTypes = [ Normal ];
+}
