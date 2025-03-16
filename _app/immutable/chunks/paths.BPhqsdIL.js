a692b3c68666e1ae8de57ccec7b06ab0c00c9597
jokk-itu <71125597+jokk-itu@users.noreply.github.com>
2025-03-16 12:38:58 +0000

    Deploying to gh-pages from @ jokk-itu/authserver-framework@1e9b14c70178dafafae72e5c04c5d4729d4753ac 🚀

--- _app/immutable/chunks/paths.BPhqsdIL.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1p2b7x1)==null?void 0:s.base)??"/authserver";var e;const t=((e=globalThis.__sveltekit_1p2b7x1)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/entry/start.sDY6suUr.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.6baWJRL-.js";export{t as start};
