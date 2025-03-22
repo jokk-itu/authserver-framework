ed3a0f38c480613f4f3017ab0691cedbf1fc4b95
joachim-kelsen <joachim@kelsen.nu>
2025-03-22 14:34:41 +0100

    add: basic pages for developer section

---
+++ doc/src/routes/developer/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+  </script>
+
+<div class="flex">
+    <nav class="mx-4 w-48 rounded-md outline-8 bg-zinc-200">
+        <ul class="mx-2 my-2 space-y-4">
+            <li class="hover:underline">
+                <a href="{base}/developer/architecture">Architecture</a>
+            </li>
+            <li class="hover:underline">
+                <a href="{base}/developer/datamodel">Datamodel</a>
+            </li>
+            <li class="hover:underline">
+                <a href="{base}/developer/client-authentication">Client Authentication</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="flex-auto"><slot /></div>
+</div>
+
+<style>
+</style>
---
+++ doc/src/routes/developer/client-authentication/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="text-4xl mb-2">Client Authentication</h1>
+<p class="mb-4">
+    Describe Client Authentication
+</p>
