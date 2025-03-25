219e9f95ede0e40aca8127f8ecf2d0d85ee33003
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-03-25 21:39:42 +0100

    add: template structure for client authentication page

--- doc/src/routes/developer/client-authentication/+page.svelte
+++ doc/src/routes/developer/client-authentication/+page.svelte
@@ -1,4 +1,27 @@
-<h1 class="text-4xl mb-2">Client Authentication</h1>
-<p class="mb-4">
-    Describe Client Authentication
-</p>
+<script>
+    import PageTitle from "../../../components/PageTitle.svelte";
+    import Section from "../../../components/Section.svelte";
+</script>
+
+<PageTitle title="Client Authentication" />
+<Section title="Introduction">
+    <p>
+        Explain about client authentication.
+        Explain why client secret jwt is not implemented.
+    </p>
+</Section>
+<Section title="Specifications">
+    <p>Show a table of specifications that are used for Client Authentication</p>
+</Section>
+<Section title="Client Secret Basic">
+    <p>Explain the method</p>
+    <p>Show an example using the HTTP code block</p>
+</Section>
+<Section title="Client Secret Post">
+    <p>Explain the method</p>
+    <p>Show an example using the HTTP code block</p>
+</Section>
+<Section title="Private key JWT">
+    <p>Explain the method</p>
+    <p>Show an example using the HTTP code block</p>
+</Section>
