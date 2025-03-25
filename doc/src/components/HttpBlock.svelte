62a1d3eacfc259bb7723154928c506eb47c02a77
joachim-kelsen <joachim@kelsen.nu>
2025-03-25 17:56:12 +0100

    add: code block components

---
+++ doc/src/components/HttpBlock.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Highlight from "svelte-highlight";
+    import http from "svelte-highlight/languages/http";
+
+    export let code: string
+</script>
+
+<Highlight language={http} code={code} />
