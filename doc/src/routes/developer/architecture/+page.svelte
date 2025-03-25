c0549d74f5a7736ac00b16234d6eae5bb5d5a100
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-03-25 19:23:13 +0100

    update: architecture with cache, endpoints and modules

--- doc/src/routes/developer/architecture/+page.svelte
+++ doc/src/routes/developer/architecture/+page.svelte
@@ -1,23 +1,63 @@
 <script>
     import PageTitle from "../../../components/PageTitle.svelte";
     import Section from "../../../components/Section.svelte";
-
-    import { base } from '$app/paths';
+    import { base } from "$app/paths";
 </script>
 
 <PageTitle title="Architecture" />
-<p>Write introduction to the architecture page</p>
+<Section title="Introduction">
+    <p>
+        The AuthServer framework has been built as a single NuGet package. It
+        offers modularity through features.
+    </p>
+</Section>
 <Section title="Endpoints">
-    <p>explain all endpoints adhere to the same pattern</p>
-    <img src="{base}/endpoint-architecture.png" alt="endpoint architecture"/>
-    <p>explain the responsibilities of the different interfaces</p>
+    <p>
+        Endpoints have been designed as a minimum HTTP handler, with an accessor
+        to parse the request, and a handler to validate and process the request.
+        <br />
+    </p>
+    <img src="{base}/endpoint-architecture.png" alt="endpoint architecture" />
+    <p>
+        <b>IRequestAccessor</b> is responsible for parsing the request into a
+        single request. It can for example contain parsed headers, query
+        parameters and body parameters.
+        <br /><br />
+        <b>RequestHandler</b> is responsible for accepting a request, then
+        validate it through its <b>IRequestValidator</b> and if valid, then
+        process it through <b>IRequestProcessor</b>.
+        <br /><br />
+        <b>IEndpointHandler</b> is the HTTP request entrypoint and is
+        responsible for handling the request by using <b>IRequestHandler</b> and
+        <b>IRequestAccessor</b>.
+    </p>
 </Section>
 <Section title="Modules">
     <p>
-        explain that functionality is wrapped in modules, and can be
-        enabled/disabled
+        The AuthServer framework consists of a single csharp project, containing
+        feature folders, and core functionality shared across features.
+        <br /><br />
+        For example, the authorize endpoint is a single feature, but some functionality
+        is shared with the pushed authorization feature.
+        <br /><br />
+        Consumers of the framework might not use all features that AuthServer has
+        to offer, and therefore each feature can be enabled or disabled.
+        <br />
+        Some features, like Dynamic Client Registration offers fine grained feature
+        control, grouped by action.
+        <br />
+        Therefore, getting a client is one feature, whereas creating a client is
+        another feature.
+        <br /><br />
+        Managing features is implemented through the Microsoft.FeatureManagement
+        library.
     </p>
 </Section>
 <Section title="Cache">
-    <p>explain that clients are cached</p>
+    <p>
+        The <b>Client</b> and related data is accessed in almost every request,
+        and that is by far mostly reading the client, as updating the client should rarely be done.
+        Therefore, to alleviate the data store, all clients are cached in a distributed cache,
+        for faster retrieval of data.
+    </p>
 </Section>
