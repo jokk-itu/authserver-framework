eeb53bcddfe07b7e3017d8f88f1c1b6b48e1caf6
joachim-kelsen <joachim@kelsen.nu>
2025-03-25 17:57:01 +0100

    update: architecture page

--- doc/src/routes/developer/+page.svelte
+++ doc/src/routes/developer/+page.svelte
@@ -1,8 +1,9 @@
 <h1 class="text-4xl mb-2">Developer Documentation</h1>
 <p class="mb-4">
-    Describes how to implement your own custom IdentityProvider using AuthServer.
-    <br/>
+    Describes how to implement your own custom IdentityProvider using
+    AuthServer.
+    <br />
     How AuthServer functions and its architectural principles.
-    <br/>
+    <br />
     The features supported by AuthServer, with integration design.
 </p>
--- doc/src/routes/developer/architecture/+page.svelte
+++ doc/src/routes/developer/architecture/+page.svelte
@@ -1,4 +1,23 @@
-<h1 class="text-4xl mb-2">Architecture</h1>
-<p class="mb-4">
-    Describe the Architecture
-</p>
+<script>
+    import PageTitle from "../../../components/PageTitle.svelte";
+    import Section from "../../../components/Section.svelte";
+
+    import { base } from '$app/paths';
+</script>
+
+<PageTitle title="Architecture" />
+<p>Write introduction to the architecture page</p>
+<Section title="Endpoints">
+    <p>explain all endpoints adhere to the same pattern</p>
+    <img src="{base}/endpoint-architecture.png" alt="endpoint architecture"/>
+    <p>explain the responsibilities of the different interfaces</p>
+</Section>
+<Section title="Modules">
+    <p>
+        explain that functionality is wrapped in modules, and can be
+        enabled/disabled
+    </p>
+</Section>
+<Section title="Cache">
+    <p>explain that clients are cached</p>
+</Section>
