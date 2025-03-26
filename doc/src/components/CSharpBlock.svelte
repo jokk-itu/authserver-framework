05488c380114f8f25fc7bc88f11853d660c2120f
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-03-26 23:16:23 +0100

    remove: unused components

--- doc/src/components/CSharpBlock.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import Highlight from "svelte-highlight";
-    import csharp from "svelte-highlight/languages/csharp";
-
-    export let code: string
-</script>
-
-<Highlight language={csharp} code={code} />
--- doc/src/components/HttpBlock.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import Highlight from "svelte-highlight";
-    import http from "svelte-highlight/languages/http";
-
-    export let code: string
-</script>
-
-<Highlight language={http} code={code} />
