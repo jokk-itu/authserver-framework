c08fb7f8a05c6bec778cfcf0f3b2498e571d763c
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-04-08 07:17:49 +0200

    update: table to accept multiple rows

--- doc/src/components/Table.svelte
+++ doc/src/components/Table.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    export let title: string
-    export let tableNumber: number
+    export let title: string;
+    export let tableNumber: number;
 
-    export let headers: string[]
-    export let rows: string[]
+    export let headers: string[];
+    export let rows: string[][];
 </script>
 
 <table class="border-collapse table-auto w-full text-sm">
@@ -13,15 +13,19 @@
     <thead class="bg-openid">
         <tr>
             {#each headers as header}
-                <th class="border font-medium p-4 pl-8 pt-3 pb-3 text-left">{header}</th>
+                <th class="border font-medium text-lg p-4 pl-8 pt-3 pb-3 text-left"
+                    >{header}</th
+                >
             {/each}
         </tr>
     </thead>
     <tbody class="bg-white">
-        <tr>
-            {#each rows as row}
-                <td class="border border-slate-200 p-4 pl-8">{row}</td>
-            {/each}
-        </tr>
+        {#each rows as row}
+            <tr>
+                {#each row as rowData}
+                    <td class="border border-slate-200 p-4 pl-8">{rowData}</td>
+                {/each}
+            </tr>
+        {/each}
     </tbody>
 </table>
