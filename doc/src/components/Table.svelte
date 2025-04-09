8caf79d59728c4eea2c8633b3172e7d313942c1f
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-04-09 19:18:35 +0200

    update: set word wrap and overflow on x axis

--- doc/src/components/Table.svelte
+++ doc/src/components/Table.svelte
@@ -6,26 +6,28 @@
     export let rows: string[][];
 </script>
 
-<table class="border-collapse table-auto w-full text-sm">
-    <caption class="caption-bottom">
-        Table {tableNumber}: {title}
-    </caption>
-    <thead class="bg-openid">
-        <tr>
-            {#each headers as header}
-                <th class="border font-medium text-lg p-4 pl-8 pt-3 pb-3 text-left"
-                    >{header}</th
-                >
-            {/each}
-        </tr>
-    </thead>
-    <tbody class="bg-white">
-        {#each rows as row}
+<div class="w-full overflow-x-auto">
+    <table class="border-collapse table-fixed w-full text-sm">
+        <caption class="caption-bottom">
+            Table {tableNumber}: {title}
+        </caption>
+        <thead class="bg-openid">
             <tr>
-                {#each row as rowData}
-                    <td class="border border-slate-200 p-4 pl-8">{rowData}</td>
+                {#each headers as header}
+                    <th class="border font-medium text-lg p-4 pl-8 pt-3 pb-3 text-left"
+                        >{header}</th
+                    >
                 {/each}
             </tr>
-        {/each}
-    </tbody>
-</table>
+        </thead>
+        <tbody class="bg-white">
+            {#each rows as row}
+                <tr>
+                    {#each row as rowData}
+                        <td class="break-words border border-slate-200 p-4 pl-8">{rowData}</td>
+                    {/each}
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
