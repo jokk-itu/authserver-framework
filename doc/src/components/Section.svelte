33e2072939ff0fa777c0ca206a27c414dc7aa0f7
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-04-14 12:15:55 +0200

    update: margin

--- doc/src/components/Section.svelte
+++ doc/src/components/Section.svelte
@@ -2,7 +2,7 @@
     export let title: String;
 </script>
 
-<div class="mb-4" id={title.toLowerCase()}>
+<div class="mb-8" id={title.toLowerCase()}>
     <div class="inline-flex">
         <a class="hover:underline ref-link text-xl" href="#{title.toLowerCase()}">{title}</a>
     </div>
