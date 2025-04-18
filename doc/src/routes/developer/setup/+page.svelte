83628db26e301b37b85fee001e2dc0703e775d39
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-04-18 10:52:18 +0200

    update: set documentation for interfaces

--- doc/src/routes/developer/setup/+page.svelte
+++ doc/src/routes/developer/setup/+page.svelte
@@ -32,12 +32,12 @@
             var rsa = RSA.Create(3072);
             var rsaSecurityKey = new RsaSecurityKey(rsa)
             {
-            KeyId = Guid.NewGuid().ToString();
+                KeyId = Guid.NewGuid().ToString();
             };
             builder.Services.AddOptions<JwksDocument>(options =>
             {
-            options.SigningKeys = [rsaSecurityKey, new SigningKey(SigningAlg.RsaSha256)];
-            options.GetTokenSigningKey = () => options.SigningKeys.Single();
+                options.SigningKeys = [rsaSecurityKey, new SigningKey(SigningAlg.RsaSha256)];
+                options.GetTokenSigningKey = () => options.SigningKeys.Single();
             });
         `}
     </CodeBlock>
@@ -58,7 +58,7 @@
                 options.ClaimsSupported = ["name", "address", "roles"];
                 options.Scopes = ["openid", "profile"];
             });
-            `}
+        `}
     </CodeBlock>
     <p>
         The UserInteraction is responsible for defining URL's for AuthServer to
@@ -78,7 +78,7 @@
                 options.LoginUri = "https://idp.authserver.dk/login";
                 options.EndSessionUri = "https://idp.authserver.dk/logout";
             });
-            `}
+        `}
     </CodeBlock>
 </Section>
 <Section title="Feature management">
@@ -92,19 +92,74 @@
             // Inside appsettings.json
             {
               "FeatureManagement": {
-                "Authorize": true,
+                "TokenIntrospection": true,
+                "TokenRevocation": true,
                 "AuthorizationCode": true,
-                "PushedAuthorization": false
+                "RefreshToken": true,
+                "ClientCredentials": true,
+                "Userinfo": true,
+                "GrantManagementRevoke": true,
+                "GrantManagementQuery": true,
+                "RegisterGet": true,
+                "RegisterDelete": true,
+                "RegisterPut": true,
+                "RegisterPost": true,
+                "EndSession": true,
+                "PushedAuthorization": true,
+                "Authorize": true,
+                "Discovery": true,
+                "Jwks": true
               }
             }
-            `}
+        `}
+    </CodeBlock>
+</Section>
+<Section title="Automatic cleanup">
+    <p>Some tables can grow very large over time, and it is therefore important to cleanup rows that cannot be used anymore.
+        There exist BackgroundServices to automatically cleanup rows for Session, AuthorizationGrant and Token tables.
+        Rows being deleted are only revoked or expired.
+        These services can be configured using the following options fields.
+    </p>
+    <CodeBlock>
+        {`
+            // Inside Program.cs
+            using AuthServer.Options;
+
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddOptions<CleanupOptions>(options =>
+            {
+                options.RunSessionCleanup = true;
+                options.SessionCleanupIntervalInSeconds = 5;
+                options.SessionCleanupBatchSize = 100;
+
+                options.RunAuthorizationGrantCleanup = true;
+                options.AuthorizationGrantCleanupIntervalInSeconds = 5;
+                options.AuthorizationGrantCleanupBatchSize = 100;
+
+                options.RunTokenCleanup = true;
+                options.RunTokenCleanupIntervalInSeconds = 5;
+                options.RunTokenCleanupBatchSize = 100;
+            });
+        `}
     </CodeBlock>
 </Section>
 <Section title="Interfaces">
-    <p>
-        Implement interface IDistibutedCache Implement interface
-        IUserClaimService Implement interface IAuthenticatedUserAccessor
-        Implement interface IAuthenticationContextReferenceResolver
+    <p>The interface IDistributedCache in namespace AuthServer.Cache.Abstractions must be implemented.
+        Its purpose is to act as a distributed cache for multi instance deployments.
+        If you only have a single instance, then an in-memory cache implementation is sufficient.
+        The cache only contains keys and values.
+    </p>
+    <p>The interface IUserClaimService in namespace AuthServer.Authentication.Abstractions must be implemented.
+        Its purpose is to return end user's claims, such that they can be used in tokens and at the userinfo endpoint.
+        This should connect to your datastore, where you persist information about end users.
+    </p>
+    <p>The interface IAuthenticatedUserAccessor in namespace AuthServer.Authentication.Abstractions must be implemented.
+        Its purpose is to provide access to currently authenticated identities, in the user's browser.
+        This is needed to determine if SSO can be performed, or if the user must choose which identity to login with.
+        This can be implemented using the cookie authentication handler provided through AspNet.Core.
+    </p>
+    <p>The interface IAuthenticationContextReferenceResolver in namespace AuthServer.Authorize.Abstractions must be implemented.
+        Its purpose is to return a AuthenticationContextReference from an AuthenticationMethodReference.
     </p>
 </Section>
 <Section title="Startup">
@@ -124,7 +179,7 @@
             app.UseAuthServer();
             
             app.Run();
-            `}
+        `}
     </CodeBlock>
 </Section>
 <Section title="Database">
@@ -133,14 +188,21 @@
         initial data must be added.
     </p>
     <br />
-    <p>AuthenticationContextReferences must be added.</p>
+    <p>
+        AuthenticationContextReferences must be added. The following example
+        inserts a single row in the AuthenticationContextReference table.
+    </p>
     <CodeBlock>
         {`
             INSERT INTO AuthenticationContextReference ([Name])
             VALUES ('urn:authserver:loa:low')
-            `}
+        `}
     </CodeBlock>
-    <p>The IdentityProvider must be added.</p>
+    <p>
+        The IdentityProvider must be added. The following example inserts
+        AuthServer as a client, and authorizes AuthServer to receive tokens with
+        scopes.
+    </p>
     <CodeBlock>
         {`
             INSERT INTO Client (
@@ -148,10 +210,10 @@
                 TokenEndpointAuthMethod, TokenEndpointAuthSigningAlg,
                 CreatedAt, AccessTokenExpiration, RequireConsent,
                 RequirePushedAuthorizationRequests, RequireReferenceToken,
-                RequireSignedRequestObject)
+                RequireSignedRequestObject, RequireIdTokenClaims)
             VALUES (
                 NEWID(), 'authserver', 'https://idp.authserver.dk',
-                0, 0, 0, GETUTCDATE(), 0, 0, 0, 0, 0)
+                0, 0, 0, GETUTCDATE(), 0, 0, 0, 0, 0, 0)
             
             DECLARE @ClientId UNIQUEIDENTIFIER = SCOPE_IDENTITY()
             
@@ -160,13 +222,16 @@
                 (@ClientId, 7), -- authserver:userinfo
                 (@ClientId, 9), -- grant_management_query
                 (@ClientId, 10) -- grant_management_revoke
-            `}
+        `}
     </CodeBlock>
-    <p>Custom Scopes must be added.</p>
+    <p>
+        Custom Scopes must be added. The following example inserts a single row
+        in the Scope table.
+    </p>
     <CodeBlock>
         {`
             INSERT INTO Scope (Name)
             VALUES ('value:of:custom:scope')
-            `}
+        `}
     </CodeBlock>
 </Section>
