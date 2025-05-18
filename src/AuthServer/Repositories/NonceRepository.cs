59e1e206d920aea8d21f44c5c16fcfb4a700bcde
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-05-18 19:46:39 +0200

    add: functions to get and create dpop nonce entities

--- src/AuthServer/Repositories/NonceRepository.cs
+++ src/AuthServer/Repositories/NonceRepository.cs
@@ -22,4 +22,37 @@ internal class NonceRepository : INonceRepository
             .Set<Nonce>()
             .AnyAsync(x => x.HashedValue == hashedNonce, cancellationToken);
     }
+
+    /// <inheritdoc/>
+    public async Task<string?> GetActiveDPoPNonce(string clientId, CancellationToken cancellationToken)
+    {
+        return await _authorizationDbContext
+            .Set<DPoPNonce>()
+            .Where(DPoPNonce.IsActive)
+            .Where(x => x.Client.Id == clientId)
+            .Select(x => x.Value)
+            .SingleOrDefaultAsync(cancellationToken);
+    }
+
+    /// <inheritdoc/>
+    public async Task<bool> IsDPoPNonce(string nonce, string clientId, CancellationToken cancellationToken)
+    {
+        var hashedNonce = nonce.Sha256();
+        return await _authorizationDbContext
+            .Set<DPoPNonce>()
+            .Where(x => x.HashedValue == hashedNonce)
+            .Where(x => x.Client.Id == clientId)
+            .AnyAsync(cancellationToken);
+    }
+
+    /// <inheritdoc/>
+    public async Task<string> CreateDPoPNonce(string clientId, CancellationToken cancellationToken)
+    {
+        var client = (await _authorizationDbContext.Set<Client>().FindAsync([clientId], cancellationToken))!;
+        var nonce = CryptographyHelper.GetRandomString(32);
+        var dDPoPNonce = new DPoPNonce(nonce, nonce.Sha256(), client);
+        client.Nonces.Add(dDPoPNonce);
+        await _authorizationDbContext.SaveChangesAsync(cancellationToken);
+        return nonce;
+    }
 }
