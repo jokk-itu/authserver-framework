6d2f76b74a2583b3fc64168f5e36a55a98af851c
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-05-18 19:25:45 +0200

    add: function to get headers for request accessors

---
+++ src/AuthServer/Extensions/IHeaderDictionaryExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace AuthServer.Extensions;
+internal static class IHeaderDictionaryExtensions
+{
+    public static string? GetValue(this IHeaderDictionary headers, string key)
+    {
+        headers.TryGetValue(key, out var value);
+        return value == StringValues.Empty ? null : value.ToString();
+    }
+}
