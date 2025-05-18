158062a52fb700ed95c8b580daebe747e627dc38
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-05-18 19:25:59 +0200

    add: function to parse http methods safely

---
+++ src/AuthServer/Extensions/HttpMethodHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace AuthServer.Extensions;
+internal static class HttpMethodHelper
+{
+    public static bool TryParse(string httpMethod, out HttpMethod? parsedHttpMethod)
+    {
+        try
+        {
+            parsedHttpMethod = HttpMethod.Parse(httpMethod);
+            return true;
+        }
+        catch (ArgumentException)
+        {
+            parsedHttpMethod = null;
+            return false;
+        }
+    }
+}
