2c9a5f208f85932ca42467a24fcad8bed413f433
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-06-08 14:19:07 +0200

    update: ignore line lengths for code blocks

--- doc/src/components/CodeBlock.svelte
+++ doc/src/components/CodeBlock.svelte
@@ -1,6 +1,6 @@
 <div class="w-full overflow-x-auto">
     <div class="outline-solid bg-gray-900 border-slate-600 border-4 rounded-lg my-8">
-        <pre class="p-4 flex overflow-x-auto leading-6">
+        <pre class="whitespace-pre p-4 flex overflow-x-auto leading-6">
             <code class="text-white pe-16">
                 <slot/>
             </code>
