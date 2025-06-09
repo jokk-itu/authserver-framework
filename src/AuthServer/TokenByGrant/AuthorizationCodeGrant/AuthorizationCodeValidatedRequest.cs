7c5288a52a300fc1b39c4a14fdeb19f66cd1c6ac
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-06-09 17:04:31 +0200

    update: validate dpop proof

--- src/AuthServer/TokenByGrant/AuthorizationCodeGrant/AuthorizationCodeValidatedRequest.cs
+++ src/AuthServer/TokenByGrant/AuthorizationCodeGrant/AuthorizationCodeValidatedRequest.cs
@@ -3,6 +3,7 @@ internal class AuthorizationCodeValidatedRequest
 {
     public required string AuthorizationGrantId { get; init; }
     public required string AuthorizationCodeId { get; init; }
+    public string? DPoPJkt { get; init; }
     public required IReadOnlyCollection<string> Scope { get; init; }
     public required IReadOnlyCollection<string> Resource { get; init; }
 }
