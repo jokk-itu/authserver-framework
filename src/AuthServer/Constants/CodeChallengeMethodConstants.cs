cfe23e6829b873c3c069d2f49239aa15f71ac1be
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-06-15 16:33:47 +0200

    add: support for sha384 and sha512 for codechallenge method

--- src/AuthServer/Constants/CodeChallengeMethodConstants.cs
+++ src/AuthServer/Constants/CodeChallengeMethodConstants.cs
@@ -2,5 +2,7 @@
 public static class CodeChallengeMethodConstants
 {
     public const string S256 = "S256";
-    public static readonly string[] CodeChallengeMethods = [S256];
+    public const string S384 = "S384";
+    public const string S512 = "S512";
+    public static readonly string[] CodeChallengeMethods = [S256, S384, S512];
 }
