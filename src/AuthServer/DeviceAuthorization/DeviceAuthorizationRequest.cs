c1f78f29bd542a7dbc8bacf274dc875e95cd9b98
joachim-kelsen <joachim@kelsen.nu>
2025-07-04 22:06:23 +0200

    add: validator

--- src/AuthServer/DeviceAuthorization/DeviceAuthorizationRequest.cs
+++ src/AuthServer/DeviceAuthorization/DeviceAuthorizationRequest.cs
@@ -1,9 +1,26 @@
 using AuthServer.Authentication.Models;
+using AuthServer.Authorization.Models;
 
 namespace AuthServer.DeviceAuthorization;
 
 internal class DeviceAuthorizationRequest
 {
+    public DeviceAuthorizationRequest(){}
+    
+    public DeviceAuthorizationRequest(AuthorizeRequestDto authorizeRequestDto, IReadOnlyCollection<ClientAuthentication> clientAuthentications, string? dPoP)
+    {
+        CodeChallenge = authorizeRequestDto.CodeChallenge;
+        CodeChallengeMethod = authorizeRequestDto.CodeChallengeMethod;
+        Nonce = authorizeRequestDto.Nonce;
+        GrantId = authorizeRequestDto.GrantId;
+        GrantManagementAction = authorizeRequestDto.GrantManagementAction;
+        Scope = authorizeRequestDto.Scope;
+        AcrValues = authorizeRequestDto.AcrValues;
+        Resource = authorizeRequestDto.Resource;
+        DPoP = dPoP;
+        ClientAuthentications = clientAuthentications;
+    }
+    
     public string? CodeChallenge { get; init; }
     public string? CodeChallengeMethod { get; init; }
     public string? Nonce { get; init; }
---
+++ src/AuthServer/DeviceAuthorization/DeviceAuthorizationRequestValidator.cs
@@ -0,0 +1,193 @@
+using AuthServer.Authentication.Abstractions;
+using AuthServer.Authorization;
+using AuthServer.Authorization.Abstractions;
+using AuthServer.Authorization.Models;
+using AuthServer.Cache.Abstractions;
+using AuthServer.Constants;
+using AuthServer.Core.Abstractions;
+using AuthServer.Core.Request;
+using AuthServer.Options;
+using AuthServer.PushedAuthorization;
+using AuthServer.Repositories.Abstractions;
+using AuthServer.TokenDecoders;
+using AuthServer.TokenDecoders.Abstractions;
+using Microsoft.Extensions.Options;
+
+namespace AuthServer.DeviceAuthorization;
+
+internal class DeviceAuthorizationRequestValidator : BaseAuthorizeValidator, IRequestValidator<DeviceAuthorizationRequest, DeviceAuthorizationValidatedRequest>
+{
+    private readonly ICachedClientStore _cachedClientStore;
+    private readonly IClientAuthenticationService _clientAuthenticationService;
+    private readonly ISecureRequestService _secureRequestService;
+    private readonly IDPoPService _dPoPService;
+
+    public DeviceAuthorizationRequestValidator(
+        ICachedClientStore cachedClientStore,
+        IClientAuthenticationService clientAuthenticationService,
+        INonceRepository nonceRepository,
+        ITokenDecoder<ServerIssuedTokenDecodeArguments> tokenDecoder,
+        IOptionsSnapshot<DiscoveryDocument> discoveryDocumentOptions,
+        ISecureRequestService secureRequestService,
+        IAuthorizationGrantRepository authorizationGrantRepository,
+        IClientRepository clientRepository,
+        IDPoPService dPoPService)
+        : base(nonceRepository, tokenDecoder, discoveryDocumentOptions, authorizationGrantRepository, clientRepository)
+    {
+        _cachedClientStore = cachedClientStore;
+        _clientAuthenticationService = clientAuthenticationService;
+        _secureRequestService = secureRequestService;
+        _dPoPService = dPoPService;
+    }
+
+    public async Task<ProcessResult<DeviceAuthorizationValidatedRequest, ProcessError>> Validate(DeviceAuthorizationRequest request, CancellationToken cancellationToken)
+    {
+        var isClientAuthenticationMethodInvalid = request.ClientAuthentications.Count != 1;
+        if (isClientAuthenticationMethodInvalid)
+        {
+            return DeviceAuthorizationError.MultipleOrNoneClientMethod;
+        }
+
+        var clientAuthentication = request.ClientAuthentications.Single();
+        var clientAuthenticationResult = await _clientAuthenticationService.AuthenticateClient(clientAuthentication, cancellationToken);
+        if (!clientAuthenticationResult.IsAuthenticated || string.IsNullOrWhiteSpace(clientAuthenticationResult.ClientId))
+        {
+            return DeviceAuthorizationError.InvalidClient;
+        }
+
+        var cachedClient = await _cachedClientStore.Get(clientAuthenticationResult.ClientId, cancellationToken);
+        if (!HasValidEmptyRequest(request.RequestObject, request.RequestUri, cachedClient.RequireSignedRequestObject))
+        {
+            return DeviceAuthorizationError.RequestOrRequestUriRequiredAsRequestObject;
+        }
+        
+        var isRequestObjectEmpty = string.IsNullOrEmpty(request.RequestObject);
+        var isRequestUriEmpty = string.IsNullOrEmpty(request.RequestUri);
+        if (!isRequestObjectEmpty && !isRequestUriEmpty)
+        {
+            return DeviceAuthorizationError.InvalidRequestAndRequestUri;
+        }
+        
+        if (!isRequestObjectEmpty)
+        {
+            var newRequest = await _secureRequestService.GetRequestByObject(request.RequestObject!, clientAuthenticationResult.ClientId, ClientTokenAudience.DeviceAuthorizationEndpoint, cancellationToken);
+            if (newRequest is null)
+            {
+                return DeviceAuthorizationError.InvalidRequest;
+            }
+
+            request = new DeviceAuthorizationRequest(newRequest, request.ClientAuthentications, request.DPoP);
+        }
+        else if (!isRequestUriEmpty)
+        {
+            if (!Uri.TryCreate(request.RequestUri, UriKind.Absolute, out var requestUri))
+            {
+                return DeviceAuthorizationError.InvalidRequestUri;
+            }
+
+            if (!cachedClient.RequestUris.Contains(requestUri.GetLeftPart(UriPartial.Path)))
+            {
+                return DeviceAuthorizationError.UnauthorizedRequestUri;
+            }
+
+            var newRequest = await _secureRequestService.GetRequestByReference(requestUri, clientAuthenticationResult.ClientId, ClientTokenAudience.DeviceAuthorizationEndpoint, cancellationToken);
+            if (newRequest is null)
+            {
+                return DeviceAuthorizationError.InvalidRequestObjectFromRequestUri;
+            }
+        }
+        
+        if (!HasDeviceCodeGrantType(cachedClient))
+        {
+            return DeviceAuthorizationError.UnauthorizedForGrant;
+        }
+
+        if (!HasValidNonce(request.Nonce))
+        {
+            return DeviceAuthorizationError.InvalidNonce;
+        }
+        
+        if (!await HasUniqueNonce(request.Nonce!, cancellationToken))
+        {
+            return DeviceAuthorizationError.ReplayNonce;
+        }
+
+        if (!HasValidCodeChallengeMethod(request.CodeChallengeMethod))
+        {
+            return DeviceAuthorizationError.InvalidCodeChallengeMethod;
+        }
+
+        if (!HasValidCodeChallenge(request.CodeChallenge))
+        {
+            return DeviceAuthorizationError.InvalidCodeChallenge;
+        }
+
+        if (!HasValidScope(request.Scope))
+        {
+            return DeviceAuthorizationError.InvalidOpenIdScope;
+        }
+
+        if (!HasAuthorizedScope(request.Scope, cachedClient))
+        {
+            return DeviceAuthorizationError.UnauthorizedScope;
+        }
+
+        if (!await HasValidResource(request.Resource, request.Scope, cancellationToken))
+        {
+            return DeviceAuthorizationError.InvalidResource;
+        }
+        
+        if (!HasValidAcrValues(request.AcrValues))
+        {
+            return DeviceAuthorizationError.InvalidAcrValues;
+        }
+        
+        if (!HasValidGrantManagementAction(request.GrantId, request.GrantManagementAction))
+        {
+            return DeviceAuthorizationError.InvalidGrantManagement;
+        }
+
+        if (!await HasValidGrantId(request.GrantId, cachedClient.Id, cancellationToken))
+        {
+            return DeviceAuthorizationError.InvalidGrantId;
+        }
+
+        if (!HasValidDPoP(null, request.DPoP, cachedClient.RequireDPoPBoundAccessTokens))
+        {
+            return DeviceAuthorizationError.DPoPRequired;
+        }
+        
+        var dPoPValidationResult = new DPoPValidationResult
+        {
+            IsValid = false
+        };
+        if (!string.IsNullOrEmpty(request.DPoP))
+        {
+            dPoPValidationResult = await _dPoPService.ValidateDPoP(request.DPoP, cachedClient.Id, cancellationToken);
+
+            if (dPoPValidationResult is { IsValid: false, DPoPNonce: null })
+            {
+                return DeviceAuthorizationError.InvalidDPoP;
+            }
+
+            if (dPoPValidationResult is { IsValid: false })
+            {
+                return DeviceAuthorizationError.UseDPoPNonce(dPoPValidationResult.DPoPNonce!);
+            }
+        }
+
+        return new DeviceAuthorizationValidatedRequest
+        {
+            ClientId = clientAuthenticationResult.ClientId,
+            AuthorizationGrantId = request.GrantId,
+            GrantManagementAction = request.GrantManagementAction,
+            CodeChallenge = request.CodeChallenge!,
+            CodeChallengeMethod = request.CodeChallengeMethod!,
+            Nonce = request.Nonce!,
+            DPoPJkt = dPoPValidationResult.DPoPJkt,
+            Scope = request.Scope,
+            Resource = request.Resource,
+            AcrValues = request.AcrValues
+        };
+    }
+}
