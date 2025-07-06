d8b077b99247df88d50da350a3e828bf7776c647
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-07-06 21:50:59 +0200

    update: move authorization to individual policies

---
+++ src/AuthServer/Authorization/OAuthToken/GrantManagementRevokePolicy.cs
@@ -0,0 +1,8 @@
+﻿using AuthServer.Authentication.OAuthToken;
+using AuthServer.Constants;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthServer.Authorization.OAuthToken;
+internal class GrantManagementRevokePolicy() : AuthorizationPolicy(
+    [new ScopeRequirement(ScopeConstants.GrantManagementRevoke)],
+    [OAuthTokenAuthenticationDefaults.AuthenticationScheme]);
---
+++ src/AuthServer/Authorization/OAuthToken/ScopeRequirementHandler.cs
@@ -0,0 +1,25 @@
+﻿using AuthServer.Constants;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthServer.Authorization.OAuthToken;
+internal class ScopeRequirementHandler : AuthorizationHandler<ScopeRequirement>
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ScopeRequirement requirement)
+    {
+        var scopes = context.User.Claims
+            .Where(x => x.Type == ClaimNameConstants.Scope)
+            .Select(x => x.Value.Split(' '))
+            .SelectMany(x => x)
+            .ToList();
+
+        var hasInsufficientScopes = !scopes.Contains(requirement.Scope);
+
+        if (hasInsufficientScopes)
+        {
+            context.Fail(new AuthorizationFailureReason(this, $"Scope is insufficient. It does not contain {requirement.Scope}"));
+        }
+
+        context.Succeed(requirement);
+        return Task.CompletedTask;
+    }
+}
