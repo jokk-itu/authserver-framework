e6af8f9dcdd9d5810e6fb2be92b7573e98aed0f9
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-07-24 18:05:55 +0200

    add: intersection and disjoint extension functions

--- src/AuthServer/Extensions/EnumerableExtensions.cs
+++ src/AuthServer/Extensions/EnumerableExtensions.cs
@@ -1,9 +1,77 @@
 ﻿namespace AuthServer.Extensions;
 internal static class EnumerableExtensions
 {
-    public static bool IsNotSubset<T>(this IEnumerable<T> subSet, IEnumerable<T> superSet)
-        => subSet.Except(superSet).Any();
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns></returns>
+    public static bool IsDisjoint<T>(this IEnumerable<T> left, IEnumerable<T> right)
+    {
+        var enumeratedSuperSet = right.ToList();
+        if (enumeratedSuperSet.Count == 0)
+        {
+            return true;
+        }
 
-    public static bool IsSubset<T>(this IEnumerable<T> subSet, IEnumerable<T> superSet)
-        => !subSet.Except(superSet).Any();
+        return !left.Intersect(enumeratedSuperSet).Any();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns></returns>
+    public static bool IsIntersected<T>(this IEnumerable<T> left, IEnumerable<T> right)
+    {
+        var enumeratedSuperSet = right.ToList();
+        if (enumeratedSuperSet.Count == 0)
+        {
+            return false;
+        }
+
+        return left.Intersect(enumeratedSuperSet).Any();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="subset"></param>
+    /// <param name="superset"></param>
+    /// <returns></returns>
+    public static bool IsSubset<T>(this IEnumerable<T> subset, IEnumerable<T> superset)
+    {
+        var enumeratedSuperSet = superset.ToList();
+        var enumeratedSubset = subset.ToList();
+        if (enumeratedSuperSet.Count == 0 || enumeratedSubset.Count == 0)
+        {
+            return false;
+        }
+
+        return !enumeratedSubset.Except(enumeratedSuperSet).Any();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="subset"></param>
+    /// <param name="superset"></param>
+    /// <returns></returns>
+    public static bool IsNotSubset<T>(this IEnumerable<T> subset, IEnumerable<T> superset)
+    {
+        var enumeratedSuperSet = superset.ToList();
+        var enumeratedSubset = subset.ToList();
+        if (enumeratedSuperSet.Count == 0 || enumeratedSubset.Count == 0)
+        {
+            return true;
+        }
+
+        return enumeratedSubset.Except(enumeratedSuperSet).Any();
+    }
 }
---
+++ tests/AuthServer.Tests.UnitTest/Extensions/EnumerableExtensionsTest.cs
@@ -0,0 +1,313 @@
+﻿using AuthServer.Extensions;
+
+namespace AuthServer.Tests.UnitTest.Extensions;
+public class EnumerableExtensionsTest
+{
+    [Fact]
+    public void IsIntersected_SingleSubset_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string> { "x" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.True(subset.IsIntersected(superset));
+    }
+
+    [Fact]
+    public void IsIntersected_SingleNotSubset_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string> { "y" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.False(subset.IsIntersected(superset));
+    }
+
+    [Fact]
+    public void IsIntersected_MultiPerfectSet_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string> { "x", "z" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.True(subset.IsIntersected(superset));
+    }
+
+    [Fact]
+    public void IsIntersected_MultiNotSubset_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string> { "y", "u" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.False(subset.IsIntersected(superset));
+    }
+
+    [Fact]
+    public void IsIntersected_MultiMixedSubset_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string> { "x", "y" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.True(subset.IsIntersected(superset));
+    }
+
+    [Fact]
+    public void IsIntersected_EmptySubset_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string>();
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.False(subset.IsIntersected(superset));
+    }
+
+    [Fact]
+    public void IsIntersected_EmptySuperset_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string> { "x" };
+        var superset = new List<string>();
+
+        // Act && Assert
+        Assert.False(subset.IsIntersected(superset));
+    }
+
+    [Fact]
+    public void IsDisjoint_SingleSubset_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string> { "x" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.False(subset.IsDisjoint(superset));
+    }
+
+    [Fact]
+    public void IsDisjoint_SingleNotSubset_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string> { "y" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.True(subset.IsDisjoint(superset));
+    }
+
+    [Fact]
+    public void IsDisjoint_MultiPerfectSet_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string> { "x", "z" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.False(subset.IsDisjoint(superset));
+    }
+
+    [Fact]
+    public void IsDisjoint_MultiNotSubset_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string> { "y", "u" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.True(subset.IsDisjoint(superset));
+    }
+
+    [Fact]
+    public void IsDisjoint_MultiMixedSubset_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string> { "x", "y" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.False(subset.IsDisjoint(superset));
+    }
+
+    [Fact]
+    public void IsDisjoint_EmptySubset_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string>();
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.True(subset.IsDisjoint(superset));
+    }
+
+    [Fact]
+    public void IsDisjoint_EmptySuperset_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string> { "x" };
+        var superset = new List<string>();
+
+        // Act && Assert
+        Assert.True(subset.IsDisjoint(superset));
+    }
+
+    [Fact]
+    public void IsSubset_SingleSubset_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string> { "x" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.True(subset.IsSubset(superset));
+    }
+
+    [Fact]
+    public void IsSubset_SingleNotSubset_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string> { "y" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.False(subset.IsSubset(superset));
+    }
+
+    [Fact]
+    public void IsSubset_MultiPerfectSet_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string> { "x", "z" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.True(subset.IsSubset(superset));
+    }
+
+    [Fact]
+    public void IsSubset_MultiNotSubset_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string> { "y", "u" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.False(subset.IsSubset(superset));
+    }
+
+    [Fact]
+    public void IsSubset_MultiMixedSubset_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string> { "x", "y" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.False(subset.IsSubset(superset));
+    }
+
+    [Fact]
+    public void IsSubset_EmptySubset_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string>();
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.False(subset.IsSubset(superset));
+    }
+
+    [Fact]
+    public void IsSubset_EmptySuperset_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string> { "x" };
+        var superset = new List<string>();
+
+        // Act && Assert
+        Assert.False(subset.IsSubset(superset));
+    }
+
+    [Fact]
+    public void IsNotSubset_SingleSubset_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string> { "x" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.False(subset.IsNotSubset(superset));
+    }
+
+    [Fact]
+    public void IsNotSubset_SingleNotSubset_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string> { "y" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.True(subset.IsNotSubset(superset));
+    }
+
+    [Fact]
+    public void IsNotSubset_MultiPerfectSet_ExpectFalse()
+    {
+        // Arrange
+        var subset = new List<string> { "x", "z" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.False(subset.IsNotSubset(superset));
+    }
+
+    [Fact]
+    public void IsNotSubset_MultiNotSubset_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string> { "y", "u" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.True(subset.IsNotSubset(superset));
+    }
+
+    [Fact]
+    public void IsNotSubset_MultiMixedSubset_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string> { "x", "y" };
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.True(subset.IsNotSubset(superset));
+    }
+
+    [Fact]
+    public void IsNotSubset_EmptySubset_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string>();
+        var superset = new List<string> { "x", "z" };
+
+        // Act && Assert
+        Assert.True(subset.IsNotSubset(superset));
+    }
+
+    [Fact]
+    public void IsNotSubset_EmptySuperset_ExpectTrue()
+    {
+        // Arrange
+        var subset = new List<string> { "x" };
+        var superset = new List<string>();
+
+        // Act && Assert
+        Assert.True(subset.IsNotSubset(superset));
+    }
+}
