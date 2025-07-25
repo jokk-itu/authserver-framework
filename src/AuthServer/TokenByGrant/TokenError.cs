6fbf5e7721bc3b4030e75d94e1f3370f48728072
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-07-25 21:51:26 +0200

    remove: unused error field

--- src/AuthServer/TokenByGrant/TokenError.cs
+++ src/AuthServer/TokenByGrant/TokenError.cs
@@ -75,9 +75,6 @@ internal static class TokenError
     public static SlowDownProcessError DeviceSlowDown(string deviceCodeId) =>
         new(deviceCodeId, ErrorCode.SlowDown, "device authorization is pending", ResultCode.BadRequest);
 
-    public static ProcessError UseDPoPNonce(string dPoPNonce)
-        => new DPoPNonceProcessError(dPoPNonce, null, ErrorCode.UseDPoPNonce, "dpop does not contain valid nonce", ResultCode.BadRequest);
-
     public static readonly ProcessError InvalidDPoP =
         new(ErrorCode.InvalidDPoPProof, "dpop is invalid", ResultCode.BadRequest);
 
