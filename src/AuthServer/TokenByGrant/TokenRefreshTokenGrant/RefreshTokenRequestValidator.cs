a94084732ad74000fe2bd9f49ad0962e4fef6417
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-07-25 21:50:46 +0200

    update: do not re-fetch active dpop nonce from dpop service

--- src/AuthServer/TokenByGrant/TokenRefreshTokenGrant/RefreshTokenRequestValidator.cs
+++ src/AuthServer/TokenByGrant/TokenRefreshTokenGrant/RefreshTokenRequestValidator.cs
@@ -107,14 +107,9 @@ internal class RefreshTokenRequestValidator : IRequestValidator<TokenRequest, Re
         if (!string.IsNullOrEmpty(request.DPoP))
         {
             dPoPValidationResult = await _dPoPService.ValidateDPoP(request.DPoP, clientId, cancellationToken);
-            if (dPoPValidationResult is { IsValid: false, DPoPNonce: null, RenewDPoPNonce: false })
+            if (dPoPValidationResult is { IsValid: false, RenewDPoPNonce: false })
             {
                 return TokenError.InvalidDPoP;
-            }
-
-            if (dPoPValidationResult is { IsValid: false, DPoPNonce: not null })
-            {
-                return TokenError.UseDPoPNonce(dPoPValidationResult.DPoPNonce!);
             }
 
             if (dPoPValidationResult is { IsValid: false, RenewDPoPNonce: true })
