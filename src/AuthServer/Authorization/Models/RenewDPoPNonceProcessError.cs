763d63506a2a03215533846a7356f4f8867af23b
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-07-25 22:04:29 +0200

    add: new process error for renewing dpop nonces

---
+++ src/AuthServer/Authorization/Models/RenewDPoPNonceProcessError.cs
@@ -0,0 +1,5 @@
+﻿using AuthServer.Core.Request;
+
+namespace AuthServer.Authorization.Models;
+internal record RenewDPoPNonceProcessError(string ClientId, string Error, string ErrorDescription, ResultCode ResultCode)
+    : ProcessError(Error, ErrorDescription, ResultCode);
