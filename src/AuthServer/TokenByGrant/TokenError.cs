9a76a1dacc42bb84427a8e6fba16c11d19165b5c
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-07-26 18:45:47 +0200

    remove: unused error field

--- src/AuthServer/TokenByGrant/TokenError.cs
+++ src/AuthServer/TokenByGrant/TokenError.cs
@@ -57,9 +57,6 @@ internal static class TokenError
     public static readonly ProcessError InvalidDPoPJktMatch =
         new(ErrorCode.InvalidDPoPProof, "dpop_jkt does not match jkt of dpop", ResultCode.BadRequest);
 
-    public static readonly ProcessError InvalidRefreshTokenJktMatch =
-        new(ErrorCode.InvalidDPoPProof, "refresh_token jkt does not match jkt of dpop", ResultCode.BadRequest);
-
     public static readonly ProcessError DPoPRequired =
         new(ErrorCode.InvalidRequest, "client requires dpop or dpop_jkt", ResultCode.BadRequest);
 
