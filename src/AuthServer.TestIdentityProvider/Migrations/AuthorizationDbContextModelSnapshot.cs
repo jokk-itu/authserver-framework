3c747b9f63a6a86b822f37fa336a00a5647e0edf
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2025-11-28 23:19:10 +0100

    update: upgrade to dotnet 10

--- src/AuthServer.TestIdentityProvider/Migrations/AuthorizationDbContextModelSnapshot.cs
+++ src/AuthServer.TestIdentityProvider/Migrations/AuthorizationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace AuthServer.TestIdentityProvider.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.11")
+                .HasAnnotation("ProductVersion", "10.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
