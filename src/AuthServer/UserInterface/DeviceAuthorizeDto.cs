50db967a29908a96505eaa26c9df946a1227c50f
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2026-01-18 13:05:05 +0100

    add: function to create or update device code grant

---
+++ src/AuthServer/UserInterface/DeviceAuthorizeDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthServer.UserInterface;
+
+public class DeviceAuthorizeDto
+{
+    public required string ClientId { get; init; }
+    public string? AuthorizationGrantId { get; init; }
+    public string? GrantManagementAction { get; init; }
+    public required string DeviceCodeId { get; init; }
+    public required string UserCodeId { get; init; }
+    public required IReadOnlyCollection<string> Scope { get; init; }
+    public IReadOnlyCollection<string> AcrValues { get; init; } = [];
+}
--- src/AuthServer/UserInterface/DeviceCodeGrantService.cs
+++ src/AuthServer/UserInterface/DeviceCodeGrantService.cs
@@ -1,12 +1,63 @@
-﻿using AuthServer.UserInterface.Abstractions;
+﻿using AuthServer.Authorize.Abstractions;
+using AuthServer.Constants;
+using AuthServer.Repositories.Abstractions;
+using AuthServer.UserInterface.Abstractions;
 
 namespace AuthServer.UserInterface;
 internal class DeviceCodeGrantService : IDeviceCodeGrantService
 {
-    public DeviceCodeGrantService()
+    private readonly IAuthenticationContextReferenceResolver _authenticationContextReferenceResolver;
+    private readonly IAuthorizationGrantRepository _authorizationGrantRepository;
+    private readonly IUserCodeRepository _userCodeRepository;
+
+    public DeviceCodeGrantService(
+        IAuthenticationContextReferenceResolver authenticationContextReferenceResolver,
+        IAuthorizationGrantRepository authorizationGrantRepository,
+        IUserCodeRepository userCodeRepository)
     {
-        
+        _authenticationContextReferenceResolver = authenticationContextReferenceResolver;
+        _authorizationGrantRepository = authorizationGrantRepository;
+        _userCodeRepository = userCodeRepository;
     }
 
-    
+    public async Task<string> HandleDeviceCodeAuthorizationGrant(string subjectIdentifier, DeviceAuthorizeDto deviceAuthorizeDto, IReadOnlyCollection<string> amr, CancellationToken cancellationToken)
+    {
+        var isCreateAction = string.IsNullOrEmpty(deviceAuthorizeDto.GrantManagementAction)
+                             || deviceAuthorizeDto.GrantManagementAction == GrantManagementActionConstants.Create;
+
+        var acr = await _authenticationContextReferenceResolver.ResolveAuthenticationContextReference(amr, cancellationToken);
+
+        if (isCreateAction)
+        {
+            var grant = await _authorizationGrantRepository.CreateDeviceCodeGrant(
+                subjectIdentifier,
+                deviceAuthorizeDto.ClientId!,
+                acr,
+                amr,
+                cancellationToken);
+
+            return grant.Id;
+        }
+        else
+        {
+            var grantId = deviceAuthorizeDto.AuthorizationGrantId;
+            if (grantId is null)
+            {
+                throw new ArgumentException("AuthorizationGrantId must not be null", nameof(deviceAuthorizeDto));
+            }
+
+            await _authorizationGrantRepository.UpdateAuthorizationGrant(
+                grantId,
+                acr,
+                amr,
+                cancellationToken);
+
+            return grantId;
+        }
+    }
+
+    public async Task RedeemUserCode(string userCode, CancellationToken cancellationToken)
+    {
+        await _userCodeRepository.RedeemUserCode(userCode, cancellationToken);
+    }
 }
