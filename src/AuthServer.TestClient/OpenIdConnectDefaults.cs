a2cab2d7fff386adbe6c98d9f9496579c1f21d59
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2026-01-18 16:38:16 +0100

    remove: test client app

--- src/AuthServer.TestClient/OpenIdConnectDefaults.cs
+++
@@ -1,8 +0,0 @@
-namespace AuthServer.TestClient;
-
-public static class OpenIdConnectDefaults
-{
-    public const string AuthenticationScheme = "openidconnect";
-    public const string DisplayName = "openidconnect";
-    public const string UserStatePropertiesKey = "userstate";
-}
