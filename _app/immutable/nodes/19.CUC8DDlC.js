c11ea20b97fcfaa3041c832782c34654c51d6fc8
jokk-itu <71125597+jokk-itu@users.noreply.github.com>
2026-03-09 19:39:07 +0000

    Deploying to gh-pages from @ jokk-itu/authserver-framework@3a44a762fac532eba27116d257dcc0cd46f10952 🚀

--- _app/immutable/nodes/19.CUC8DDlC.js
+++ _app/immutable/nodes/19.CUC8DDlC.js
@@ -1,6 +1,6 @@
-import{f as h,a as e,t as l}from"../chunks/BHF9wNqu.js";import"../chunks/CU9afjlv.js";import{f as A,e as E,s as t,$ as D,n as s}from"../chunks/DL6eumzt.js";import{h as z}from"../chunks/DMOXgEdX.js";import{C as p}from"../chunks/DbZgjyKz.js";import{I as w}from"../chunks/CuMYCe1t.js";import{P as O,S as m}from"../chunks/C3UgRdqy.js";var U=h(`<p>The following sections describe how to setup AuthServer framework in
+import{f as v,a as e,t as p}from"../chunks/BHF9wNqu.js";import"../chunks/CU9afjlv.js";import{f,e as E,s as n,$ as z,n as a}from"../chunks/DL6eumzt.js";import{h as O}from"../chunks/DMOXgEdX.js";import{C as h}from"../chunks/DbZgjyKz.js";import{I as S}from"../chunks/CuMYCe1t.js";import{P as U,S as g}from"../chunks/C3UgRdqy.js";var B=v(`<p>The following sections describe how to setup AuthServer framework in
         your solution. <br/> The example code is written for an AspNetCore WebApp in .NET 10. <br/> If you don't want to read the setup documentation, but just want to head directly into the code,
-        then take a look at the MVP setup in <a href="https://github.com/jokk-itu/authserver-framework/tree/main/src/AuthServer.TestIdentityProvider">AuthServer.TestIdentityProvider</a>.</p>`),G=h(`<p>The JwksDocument is responsible for defining keys for signing and
+        then take a look at the MVP setup in <a href="https://github.com/jokk-itu/authserver-framework/tree/main/src/AuthServer.TestIdentityProvider">AuthServer.TestIdentityProvider</a>.</p>`),G=v(`<p>The JwksDocument is responsible for defining keys for signing and
         encrypting tokens.</p> <!> <!> <p>The DiscoveryDocument is responsible for defining metadata about your
         AuthServer instance. It is only customizable properties that are
         exposed, as some are internally handled.</p> <!> <p>The UserInteraction is responsible for defining URL's for AuthServer to
@@ -8,24 +8,24 @@ import{f as h,a as e,t as l}from"../chunks/BHF9wNqu.js";import"../chunks/CU9afjl
         redirecting from the authorize endpoint or the end-session endpoint.</p> <!> <p>If your deployment is distributed, such that you have multiple deployments of AuthServer, then it is important to handle clock drifting.
         This is important as creating tokens and verifying tokens can happen on different machines.
         The token validation can be customized to allow a ClockSkew of a custom TimeSpan.</p> <!> <p>Endpoints can be customized using the EndpointOptions.
-        For example, to require authentication for POST in the register endpoint.</p> <!>`,1),B=h(`<p>The modules in AuthServer are built to be dynamically turned on or off
+        For example, to require authentication for POST in the register endpoint.</p> <!>`,1),M=v(`<p>The modules in AuthServer are built to be dynamically turned on or off
         during runtime. This is handled through the library
-        Microsoft.FeatureManagement.</p> <!>`,1),N=h(`<p>Some tables can grow very large over time, and it is therefore important to cleanup rows that cannot be used anymore.
+        Microsoft.FeatureManagement.</p> <!>`,1),N=v(`<p>Some tables can grow very large over time, and it is therefore important to cleanup rows that cannot be used anymore.
         There exist BackgroundServices to automatically cleanup rows for Session, AuthorizationGrant and Token tables.
         Rows being deleted are only revoked or expired.
-        These services can be configured using the following options fields.</p> <!>`,1),M=h(`<b>AuthServer.Cache.Abstractions.IDistributedCache</b> <p>Its purpose is to act as a distributed cache for multi instance deployments.
+        These services can be configured using the following options fields.</p> <!>`,1),V=v(`<b>AuthServer.Cache.Abstractions.IDistributedCache</b> <p>Its purpose is to act as a distributed cache for multi instance deployments.
         If you only have a single instance, then an in-memory cache implementation is sufficient.
         The cache only contains keys and values.</p> <p>An implementation is required for the interface, as the cache is a core component of the framework.</p> <p>An example implementation using Redis.StackExchange can be seen below.</p> <!> <br/> <b>AuthServer.Authentication.Abstractions.IUserClaimService</b> <p>Its purpose is to return end user's claims, such that they can be used in tokens and at the userinfo endpoint.
         This should connect to your datastore, where you persist information about end users.</p> <p>An implementation is required if end users are supported in your custom AuthServer implementation.</p> <br/> <b>AuthServer.Authentication.Abstractions.IAuthenticatedUserAccessor</b> <p>Its purpose is to provide access to currently authenticated identities, in the user's browser.
         This is needed to determine if SSO can be performed, or if the user must choose which identity to login with.
-        This can be implemented using the cookie authentication handler provided through AspNet.Core.</p> <p>An implementation is required if end users are supported in your custom AuthServer implementation.</p> <br/> <b>AuthServer.Authorize.Abstractions.IAuthenticationContextReferenceResolver</b> <p>Its purpose is to return an AuthenticationContextReference from an AuthenticationMethodReference.</p> <br/> <b>AuthServer.TokenByGrant.TokenExchangeGrant.Abstractions.IExtendedTokenExchangeRequestValidator</b> <p>Its purpose is to allow for customizing validation during token exchange grant type at the token endpoint.</p>`,1),V=h(`<p>The following example shows how to add services for AuthServer, and how
-        to setup AuthServer in the AspNetCore HTTP request pipeline.</p> <!>`,1),q=h("<p>Remember to change the ClientUri to your deployment URL.</p>"),K=h(`<p>Once migrations have been created and applied to the database, the
+        This can be implemented using the cookie authentication handler provided through AspNet.Core.</p> <p>An implementation is required if end users are supported in your custom AuthServer implementation.</p> <br/> <b>AuthServer.Authorize.Abstractions.IAuthenticationContextReferenceResolver</b> <p>Its purpose is to return an AuthenticationContextReference from an AuthenticationMethodReference.</p> <br/> <b>AuthServer.TokenByGrant.TokenExchangeGrant.Abstractions.IExtendedTokenExchangeRequestValidator</b> <p>Its purpose is to allow for customizing validation during token exchange grant type at the token endpoint.</p>`,1),q=v(`<p>The following example shows how to add services for AuthServer, and how
+        to setup AuthServer in the AspNetCore HTTP request pipeline.</p> <!>`,1),H=v("<p>Remember to change the ClientUri to your deployment URL.</p>"),L=v(`<p>Once migrations have been created and applied to the database, the
         initial data must be added.</p> <br/> <p>AuthenticationContextReferences must be added. The following example
         inserts a single row in the AuthenticationContextReference table.</p> <!> <p>The IdentityProvider must be added. The following example inserts
         AuthServer as a client, and authorizes AuthServer to receive tokens with
         scopes.</p> <!> <!> <p>Custom Scopes can be added. The following example inserts a single row
-        in the Scope table.</p> <!>`,1),L=h(`<p>There are heplful services in the framework, which can be used to make authorization grants and consent, used during end user authentication.
-        For example, during authorize of the authorization_code grant type or during redeeming the user code of device_code grant type.</p> <p>The following interfaces can be injected and used. They all exist in the namespace AuthServer.UserInterface.Abstractions.</p> <ul><li>IAuthorizationCodeGrantService</li> <li>IAuthorizeService</li> <li>IConsentGrantService</li> <li>IDeviceAuthorizeService</li> <li>IDeviceCodeGrantService</li> <li>IEndSessionService</li></ul>`,1),W=h("<!> <!> <!> <!> <!> <!> <!> <!> <!>",1);function Z(R){var b=W();z("qwvjmj",o=>{E(()=>{D.title="IdentityProvider setup overview"})});var C=A(b);O(C,{title:"Setup"});var $=t(C,2);m($,{title:"Introduction",children:(o,g)=>{var r=U();e(o,r)},$$slots:{default:!0}});var T=t($,2);m(T,{title:"Options",children:(o,g)=>{var r=G(),u=t(A(r),2);w(u,{children:(n,S)=>{s();var a=l("There must only be registered one key per algorithm. The algorithm is set in the constructor of the key.");e(n,a)},$$slots:{default:!0}});var d=t(u,2);p(d,{children:(n,S)=>{s();var a=l();a.nodeValue=`
+        in the Scope table.</p> <!>`,1),W=v(`<p>There are heplful services in the framework, which can be used to make authorization grants and consent, used during end user authentication.
+        For example, during authorize of the authorization_code grant type or during redeeming the user code of device_code grant type.</p> <p>The following interfaces can be injected and used. They all exist in the namespace AuthServer.UserInterface.Abstractions.</p> <b>IAuthorizationCodeGrantService</b> <p>Service used to create/update grants during the grant type authorization_code.</p> <br/> <b>IAuthorizeService</b> <p>Service used for general functionality during the authorize endpoint.</p> <br/> <b>IConsentGrantService</b> <p>Service used for granting and getting consents of end-users.</p> <br/> <b>IDeviceAuthorizeService</b> <p>Service used for general functionality during the device endpoint, used for redeeming the user code.</p> <br/> <b>IDeviceCodeGrantService</b> <p>Service used to create/update grants during the grant type device_code.</p> <br/> <b>IEndSessionService</b> <p>Service used for general functionality during the end session endpoint.</p> <br/>`,1),F=v("<p>Requesting clients from AuthServer, such as during backchannel logout, uses HttpClients and they can be extended.</p> <p>During the setup at Program.cs, you can pass an Action where you can configure the HttpClient.</p> <p>It is setup per integration, such as backchannel logout.</p> <!> <!>",1),K=v("<!> <!> <!> <!> <!> <!> <!> <!> <!> <!>",1);function ne(R){var C=K();O("qwvjmj",o=>{E(()=>{z.title="IdentityProvider setup overview"})});var $=f(C);U($,{title:"Setup"});var y=n($,2);g(y,{title:"Introduction",children:(o,m)=>{var t=B();e(o,t)},$$slots:{default:!0}});var T=n(y,2);g(T,{title:"Options",children:(o,m)=>{var t=G(),s=n(f(t),2);S(s,{children:(r,b)=>{a();var u=p("There must only be registered one key per algorithm. The algorithm is set in the constructor of the key.");e(r,u)},$$slots:{default:!0}});var d=n(s,2);h(d,{children:(r,b)=>{a();var u=p();u.nodeValue=`
 // Inside Program.cs
 using AuthServer.Options;
 using AuthServer.Enums;
@@ -43,7 +43,7 @@ builder.Services
         options.SigningKeys = [rsaSecurityKey, new SigningKey(SigningAlg.RsaSha256)];
         options.GetTokenSigningKey = () => options.SigningKeys.Single();
     });
-`,e(n,a)},$$slots:{default:!0}});var v=t(d,4);p(v,{children:(n,S)=>{s();var a=l();a.nodeValue=`
+`,e(r,u)},$$slots:{default:!0}});var c=n(d,4);h(c,{children:(r,b)=>{a();var u=p();u.nodeValue=`
 // Inside Program.cs
 using AuthServer.Options;
             
@@ -56,7 +56,7 @@ builder.Services
         options.ClaimsSupported = ["name", "address", "roles"];
         options.Scopes = ["openid", "profile"];
     });
-`,e(n,a)},$$slots:{default:!0}});var i=t(v,4);p(i,{children:(n,S)=>{s();var a=l();a.nodeValue=`
+`,e(r,u)},$$slots:{default:!0}});var i=n(c,4);h(i,{children:(r,b)=>{a();var u=p();u.nodeValue=`
 // Inside Program.cs
 using AuthServer.Options;
             
@@ -71,7 +71,7 @@ builder.Services
         options.EndSessionUri = "https://idp.authserver.dk/logout";
         options.VerificationUri = "https://idp.authserver.dk/device";
     });
-`,e(n,a)},$$slots:{default:!0}});var c=t(i,4);p(c,{children:(n,S)=>{s();var a=l();a.nodeValue=`               
+`,e(r,u)},$$slots:{default:!0}});var l=n(i,4);h(l,{children:(r,b)=>{a();var u=p();u.nodeValue=`               
 // Inside Program.cs
 using AuthServer.Options;
             
@@ -82,7 +82,7 @@ builder.Services
     {
         options.ClockSkew = TimeSpan.FromSeconds(10);
     });
-`,e(n,a)},$$slots:{default:!0}});var f=t(c,4);p(f,{children:(n,S)=>{s();var a=l();a.nodeValue=`
+`,e(r,u)},$$slots:{default:!0}});var A=n(l,4);h(A,{children:(r,b)=>{a();var u=p();u.nodeValue=`
 // Inside Program.cs
 using AuthServer.Options;
 
@@ -96,7 +96,7 @@ builder.Services
     {
         options.ClientRegistrationAuthenticationScheme = "NameOfAuthenticationScheme";
     });
-`,e(n,a)},$$slots:{default:!0}}),e(o,r)},$$slots:{default:!0}});var y=t(T,2);m(y,{title:"Feature management",children:(o,g)=>{var r=B(),u=t(A(r),2);p(u,{children:(d,v)=>{s();var i=l();i.nodeValue=`
+`,e(r,u)},$$slots:{default:!0}}),e(o,t)},$$slots:{default:!0}});var k=n(T,2);g(k,{title:"Feature management",children:(o,m)=>{var t=M(),s=n(f(t),2);h(s,{children:(d,c)=>{a();var i=p();i.nodeValue=`
 // Inside appsettings.json
 {
   "FeatureManagement": {
@@ -122,7 +122,7 @@ builder.Services
     "Jwks": true
   }
 }
-`,e(d,i)},$$slots:{default:!0}}),e(o,r)},$$slots:{default:!0}});var k=t(y,2);m(k,{title:"Automatic cleanup",children:(o,g)=>{var r=N(),u=t(A(r),2);p(u,{children:(d,v)=>{s();var i=l();i.nodeValue=`
+`,e(d,i)},$$slots:{default:!0}}),e(o,t)},$$slots:{default:!0}});var I=n(k,2);g(I,{title:"Automatic cleanup",children:(o,m)=>{var t=N(),s=n(f(t),2);h(s,{children:(d,c)=>{a();var i=p();i.nodeValue=`
 // Inside Program.cs
 using AuthServer.Options;
 
@@ -141,7 +141,7 @@ builder.Services.AddOptions<CleanupOptions>(options =>
   options.RunTokenCleanupIntervalInSeconds = 5;
   options.RunTokenCleanupBatchSize = 100;
 });
-`,e(d,i)},$$slots:{default:!0}}),e(o,r)},$$slots:{default:!0}});var I=t(k,2);m(I,{title:"Interfaces",children:(o,g)=>{var r=M(),u=t(A(r),8);p(u,{children:(d,v)=>{s();var i=l();i.nodeValue=`
+`,e(d,i)},$$slots:{default:!0}}),e(o,t)},$$slots:{default:!0}});var _=n(I,2);g(_,{title:"Interfaces",children:(o,m)=>{var t=V(),s=n(f(t),8);h(s,{children:(d,c)=>{a();var i=p();i.nodeValue=`
 using AuthServer.Cache.Abstractions;
 using System.Text.Json;
 
@@ -174,7 +174,7 @@ public class DistributedCache : IDistributedCache
         await database.KeyDeleteAsync(key);
     }
 }
-        `,e(d,i)},$$slots:{default:!0}}),s(28),e(o,r)},$$slots:{default:!0}});var _=t(I,2);m(_,{title:"Startup",children:(o,g)=>{var r=V(),u=t(A(r),2);p(u,{children:(d,v)=>{s();var i=l();i.nodeValue=`
+        `,e(d,i)},$$slots:{default:!0}}),a(28),e(o,t)},$$slots:{default:!0}});var x=n(_,2);g(x,{title:"Startup",children:(o,m)=>{var t=q(),s=n(f(t),2);h(s,{children:(d,c)=>{a();var i=p();i.nodeValue=`
 // Inside Program.cs
 var builder = WebApplication.CreateBuilder(args);
 builder.Services
@@ -217,10 +217,10 @@ var app = builder.Build();
 app.UseAuthServer();
             
 app.Run();
-`,e(d,i)},$$slots:{default:!0}}),e(o,r)},$$slots:{default:!0}});var x=t(_,2);m(x,{title:"Database",children:(o,g)=>{var r=K(),u=t(A(r),6);p(u,{children:(c,f)=>{s();var n=l();n.nodeValue=`
+`,e(d,i)},$$slots:{default:!0}}),e(o,t)},$$slots:{default:!0}});var w=n(x,2);g(w,{title:"Database",children:(o,m)=>{var t=L(),s=n(f(t),6);h(s,{children:(l,A)=>{a();var r=p();r.nodeValue=`
 INSERT INTO AuthenticationContextReference ([Name])
 VALUES ('urn:authserver:loa:low')
-        `,e(c,n)},$$slots:{default:!0}});var d=t(u,4);w(d,{children:(c,f)=>{var n=q();e(c,n)},$$slots:{default:!0}});var v=t(d,2);p(v,{children:(c,f)=>{s();var n=l();n.nodeValue=`
+        `,e(l,r)},$$slots:{default:!0}});var d=n(s,4);S(d,{children:(l,A)=>{var r=H();e(l,r)},$$slots:{default:!0}});var c=n(d,2);h(c,{children:(l,A)=>{a();var r=p();r.nodeValue=`
 INSERT INTO Client (
   Id, [Name], ClientUri, ApplicationType, 
   TokenEndpointAuthMethod, TokenEndpointAuthSigningAlg,
@@ -238,7 +238,17 @@ VALUES
   (@ClientId, 7), -- authserver:userinfo
   (@ClientId, 9), -- grant_management_query
   (@ClientId, 10) -- grant_management_revoke
-`,e(c,n)},$$slots:{default:!0}});var i=t(v,4);p(i,{children:(c,f)=>{s();var n=l();n.nodeValue=`
+`,e(l,r)},$$slots:{default:!0}});var i=n(c,4);h(i,{children:(l,A)=>{a();var r=p();r.nodeValue=`
 INSERT INTO Scope (Name)
 VALUES ('value:of:custom:scope')
-`,e(c,n)},$$slots:{default:!0}}),e(o,r)},$$slots:{default:!0}});var P=t(x,2);m(P,{title:"Authorization and Consent",children:(o,g)=>{var r=L();s(4),e(o,r)},$$slots:{default:!0}}),e(R,b)}export{Z as component};
+`,e(l,r)},$$slots:{default:!0}}),e(o,t)},$$slots:{default:!0}});var P=n(w,2);g(P,{title:"Authorization and Consent",children:(o,m)=>{var t=W();a(38),e(o,t)},$$slots:{default:!0}});var D=n(P,2);g(D,{title:"HttpClient configurations",children:(o,m)=>{var t=F(),s=n(f(t),6);S(s,{children:(c,i)=>{a();var l=p("It is very important to configure the HttpClients, and the default setup configures a strict Timeout and response Content size. This limits the attack surface of Denial of Service.");e(c,l)},$$slots:{default:!0}});var d=n(s,2);h(d,{children:(c,i)=>{a();var l=p();l.nodeValue=`
+// Inside Program.cs
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddAuthServer()
+    .AddLogoutHttpClient(httpClient =>
+    {
+        httpClient.Timeout = TimeSpan.FromSeconds(2);
+        httpClient.MaxResponseContentBufferSize = 1024 * 32;
+    });
+`,e(c,l)},$$slots:{default:!0}}),e(o,t)},$$slots:{default:!0}}),e(R,C)}export{ne as component};
