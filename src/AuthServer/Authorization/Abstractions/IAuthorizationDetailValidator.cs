f0528cfb3b8a5b29ec540c614b8953cda6b55ee1
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2026-04-06 18:02:29 +0200

    add: interface for implementers to validate authorization details

---
+++ src/AuthServer/Authorization/Abstractions/IAuthorizationDetailValidator.cs
@@ -0,0 +1,14 @@
+﻿using AuthServer.Authorization.Models;
+
+namespace AuthServer.Authorization.Abstractions;
+
+public interface IAuthorizationDetailValidator
+{
+    /// <summary>
+    /// Validates an authorization_detail entity from the authorization_details parameter during authorization.
+    /// </summary>
+    /// <param name="authorizationDetail">A JSON encoded AuthorizationDetail</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>The validated AuthorizationDetail, or null if invalid.</returns>
+    Task<AuthorizationDetailDto?> ValidateAuthorizationDetail(string authorizationDetail, CancellationToken cancellationToken);
+}
---
+++ src/AuthServer/Authorization/Models/AuthorizationDetailDto.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AuthServer.Authorization.Models;
+
+public abstract class AuthorizationDetailDto
+{
+    /// <summary>
+    /// The type field. It is required.
+    /// <remarks>https://datatracker.ietf.org/doc/html/rfc9396#name-authorization-details-types</remarks>
+    /// </summary>
+    [JsonPropertyName("type")]
+    public required string Type { get; init; }
+
+    /// <summary>
+    /// The locations field. It is optional.
+    /// <remarks>https://datatracker.ietf.org/doc/html/rfc9396#name-common-data-fields</remarks>
+    /// </summary>
+    [JsonPropertyName("locations")]
+    public IReadOnlyCollection<string> Locations { get; set; } = [];
+}
