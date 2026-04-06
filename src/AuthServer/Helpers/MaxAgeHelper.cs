9d368fd3f82979a5b6bdb2d69fe81525f38d76ab
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2026-04-06 18:00:41 +0200

    update: improve readability

--- src/AuthServer/Helpers/MaxAgeHelper.cs
+++ src/AuthServer/Helpers/MaxAgeHelper.cs
@@ -5,7 +5,7 @@ internal static class MaxAgeHelper
     public static bool IsMaxAgeValid(string? maxAge)
     {
         return string.IsNullOrWhiteSpace(maxAge)
-               || int.TryParse(maxAge, out var parsedMaxAge)
-               && parsedMaxAge > -1;
+               || (int.TryParse(maxAge, out var parsedMaxAge)
+                   && parsedMaxAge > -1);
     }
 }
