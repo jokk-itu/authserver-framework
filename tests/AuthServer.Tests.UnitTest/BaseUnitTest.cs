538c3f4e1b84dfb0f7bdbc05d57bf184c6473893
DESKTOP-CGIK2PI\Joachim <joachim@kelsen.nu>
2026-05-24 14:22:28 +0200

    update: re-introduce rsa

--- tests/AuthServer.Tests.UnitTest/BaseUnitTest.cs
+++ tests/AuthServer.Tests.UnitTest/BaseUnitTest.cs
@@ -82,8 +82,8 @@ public abstract class BaseUnitTest
             options.AcrValuesSupported =
                 [LevelOfAssuranceLow, LevelOfAssuranceSubstantial, LevelOfAssuranceStrict];
 
-            var supportedSigningAlgorithms = new[] { JwsAlgConstants.RsaSsaPssSha256, JwsAlgConstants.EcdsaSha256 };
-            var supportedEncryptionAlgorithms = new[] { JweAlgConstants.RsaOAEP, JweAlgConstants.EcdhEsA128KW };
+            var supportedSigningAlgorithms = new[] { JwsAlgConstants.RsaSha256, JwsAlgConstants.RsaSsaPssSha256, JwsAlgConstants.EcdsaSha256 };
+            var supportedEncryptionAlgorithms = new[] { JweAlgConstants.RsaPKCS1, JweAlgConstants.RsaOAEP, JweAlgConstants.EcdhEsA128KW };
             var supportedEncryptionEncoding = new[] { JweEncConstants.Aes128CbcHmacSha256 };
 
             options.IntrospectionEndpointAuthSigningAlgValuesSupported = supportedSigningAlgorithms;
